af8cd475c846d77f719bacec416e3c857ef21cb6
corentin <corentin.girard@datadoghq.com>
2023-04-07 13:23:07 +0200

    fix

--- .github/workflows/test.js
+++
@@ -1,4 +0,0 @@
-module.exports = ({ github, context }) => {
-  console.log("hello");
-  return context.payload.client_payload.value;
-};
---
+++ ci/release-pr-comment.js
@@ -0,0 +1,2 @@
+module.exports = `Once merged, this PR will automatically create a GitHub release for you.
+The description of the release will exactly match this PR's description! Feel free to edit it.`;
