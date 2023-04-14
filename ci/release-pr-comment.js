a2f69472e9cf03b28e2bee16e8d58ad3785cc665
corentin <corentin.girard@datadoghq.com>
2023-04-14 11:31:21 +0200

    fix

--- ci/release-pr-comment.js
+++
@@ -1,2 +0,0 @@
-module.exports = `Once merged, this PR will automatically create a GitHub release for you.
-The description of the release will exactly match this PR's description! Feel free to edit it.`;
